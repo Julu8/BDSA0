61403e83f11f723184bfa9e5debb830ba24e1115
Maj Frost Jensen <maj@frost-jensen.dk>
2021-09-06 19:02:01 +0200

    Created user interface for isLeapYear

--- MyApp/LeapYearChecker/Program.cs
+++ MyApp/LeapYearChecker/Program.cs
@@ -4,22 +4,33 @@ namespace LeapYearChecker
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter Year: ");
+            string input = Console.ReadLine();
+            int year = Convert.ToInt32(input);
+            var testYear = new Program(); 
+            testYear.isLeapYear(year);
         }
 
         public bool isLeapYear(int year){
             if ((year % 400) == 0){
+                Console.WriteLine("Yay");
                 return true;
             }
             else if ((year % 100) == 0){
+                Console.WriteLine("Nay");
                 return false;
             }
             else if ((year % 4) == 0){
+                Console.WriteLine("Yay");
                 return true;
             }
-            else return false;
+            
+            else {
+                Console.WriteLine("Nay");
+                return false;
+            }
         }
 
     }
--- MyApp/LeapYearTester.Tests/UnitTest1.cs
+++ MyApp/LeapYearTester.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 using LeapYearChecker;
 
@@ -6,6 +7,7 @@ namespace LeapYearTester.Tests
 {
     public class UnitTest1
     {
+
         [Theory]
         [InlineData(1)]
         [InlineData(100)]
@@ -24,5 +26,43 @@ namespace LeapYearTester.Tests
             var result = new LeapYearChecker.Program().isLeapYear(year);
             Assert.True(result, $"{year} should be a leap year.");
         }
+        
+        [Theory]
+        [InlineData("1")]
+        [InlineData("6")]
+        [InlineData("100")]
+        [InlineData("200")]
+        public void UserTestFalse(string year){
+            var writer = new StringWriter();
+            var input = new StringReader(year);
+            
+            Console.SetOut(writer);
+            Console.SetIn(input);
+
+            LeapYearChecker.Program.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            Assert.Equal($"Enter Year: Nay", output);
+        }
+
+        [Theory]
+        [InlineData("4")]
+        [InlineData("400")]
+        [InlineData("800")]
+        public void UserTestTrue(string year){
+            var writer = new StringWriter();
+            var input = new StringReader(year);
+            
+            Console.SetOut(writer);
+            Console.SetIn(input);
+
+            LeapYearChecker.Program.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            Assert.Equal($"Enter Year: Yay", output);
+        }
+
     }
 }
